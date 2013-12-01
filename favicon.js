f9014f3e5feffd46b56819bb072f63748fc17ad6
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-12-01 18:13:52 -0500

    Now we can accept multiple arguments and return TSV output

--- favicon.js
+++ favicon.js
@@ -8,82 +8,101 @@ if (args.length < 2) {
 	phantom.exit();
 }
 
-var url = 'http://www.' + args[1] + '/';
-
 // local jQuery to inject on the page
 var jquery = 'jquery-1.10.2.min.js';
 
-var page = require('webpage').create();
-page.settings.loadImages = false;
+var getFavicon = function(domain, callback) {
+	var url = 'http://www.' + domain + '/';
 
-// disabling output of errors on the page
-page.onError = function(msg, trace) {};
+	var page = require('webpage').create();
+	page.settings.loadImages = false;
 
-var acceptable_urls = [ url ];
-/*
- * If page was redirected, add redirect URL to a list of allowed requests
- */
-page.onResourceReceived = function(response) {
-	for (var i=0; i < acceptable_urls.length; i++)
-	{
-		if (response.url == acceptable_urls[i] && response.redirectURL) {
-			acceptable_urls.push(response.redirectURL);
-			break;
+	// disabling output of errors on the page
+	page.onError = function(msg, trace) {};
+
+	var acceptable_urls = [ url ];
+	/*
+	 * If page was redirected, add redirect URL to a list of allowed requests
+	 */
+	page.onResourceReceived = function(response) {
+		for (var i=0; i < acceptable_urls.length; i++)
+		{
+			if (response.url == acceptable_urls[i] && response.redirectURL) {
+				acceptable_urls.push(response.redirectURL);
+				break;
+			}
 		}
-	}
+	};
+
+	/*
+	 * If it's not an original URL or a page it was redirected to, don't make a request
+	 */
+	page.onResourceRequested = function(requestData, request) {
+		var url_is_ok = false;
+		for (var i=0; i < acceptable_urls.length; i++)
+		{
+			if (requestData['url'] == acceptable_urls[i]) {
+				url_is_ok = true;
+				break;
+			}
+		}
+
+		if (!url_is_ok) {
+			request.abort();
+		}
+	};
+
+	page.open(url, function(status) {
+		if (status !== 'success') {
+			return false;
+		}
+
+		page.injectJs(jquery);
+
+		var favicon = page.evaluate(function() {
+			var href = false;
+
+			$('link').each(function() {
+				var rel = $(this).attr('rel');
+				if (rel == 'icon' || rel == 'shortcut icon') {
+					href = $(this).attr('href');
+
+					// resolving relative URL
+					var a = document.createElement('a');
+					a.href = href;
+					href = a.href;
+
+					return;
+				}
+			});
+
+			return href;
+		});
+
+		if (!favicon) {
+			callback(domain, url + 'favicon.ico');
+		} else {
+			callback(domain, favicon);
+		}
+	});
 };
 
-/*
- * If it's not an original URL or a page it was redirected to, don't make a request
- */
-page.onResourceRequested = function(requestData, request) {
-	var url_is_ok = false;
-	for (var i=0; i < acceptable_urls.length; i++)
-	{
-		if (requestData['url'] == acceptable_urls[i]) {
-			url_is_ok = true;
-			break;
-		}
+var i=1;
+
+var chainCaller = function(domain, favicon) {
+	// if we're invoked as callback, print the result
+	if (domain && favicon) {
+		console.log(domain + '\t' + favicon);
 	}
 
-	if (!url_is_ok) {
-		request.abort();
-	}
-};
-
-page.open(url, function(status) {
-	if (status !== 'success') {
+	if (i < args.length) {
+		getFavicon(args[i], chainCaller);
+	} else {
 		phantom.exit();
 	}
 
-	page.injectJs(jquery);
+	i++;
+}
 
-	var favicon = page.evaluate(function() {
-		var href = false;
-
-		$('link').each(function() {
-			var rel = $(this).attr('rel');
-			if (rel == 'icon' || rel == 'shortcut icon') {
-				href = $(this).attr('href');
-
-				// resolving relative URL
-				var a = document.createElement('a');
-				a.href = href;
-				href = a.href;
-
-				return;
-			}
-		});
-
-		return href;
-	});
-
-	if (!favicon) {
-		console.log(url + 'favicon.ico');
-	} else {
-		console.log(favicon);
-	}
-
-	phantom.exit();
-});
-
+// starting the chain
+chainCaller();
